279f9048a7adca1ce8d5ea1adb958edab15fc0e5
tinoak <tinoak@mac.com>
2011-05-29 16:46:32 +0200

    validate image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,16 +2,12 @@ class Photo < ActiveRecord::Base
     
     attr_accessible :username, :imagestring, :image, :image_file_name, :image_content_type, :image_file_size
     
-    #validates :username, :presence => true
-    
-    #validates :imagestring, :presence => true
-    
-    
+    validates_attachment_presence :image
     
     has_attached_file :image, :url => "/:class/:attachment/:id/:style_:basename.:extension",  
                               :styles => { :original => "460x460^", :normal => "360x360^", :small => "180x180^" }, 
                               :convert_options => {:all => '-auto-orient' },
-    :storage => :s3,
-    :s3_credentials => S3_CREDENTIALS 
+                              :storage => :s3,
+                              :s3_credentials => S3_CREDENTIALS 
     
 end
